2199094bbbfaaa04fccbb0118fa47f21bfd6dd02
Evgeny Khavdey <khavdey61@yandex.ru>
2021-07-28 15:45:00 +0300

    Initial commit

---
+++ desktop/src/ru/khavdey/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.khavdey.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.khavdey.StarGames;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGames(), config);
+	}
+}
